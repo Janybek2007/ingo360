18cef36cf24c30687dd25828b3cfedd3082eacab
Abdurakhman <abdibaitovabdurahman@gmail.com>
2025-10-14 12:13:54 +0600

    BugFix/ Исправлено ошибка связанная с добавлением данных (новая компания)

--- frontend/src/features/company/company.contract.ts
+++ frontend/src/features/company/company.contract.ts
@@ -3,7 +3,10 @@ import { z } from 'zod';
 export const AddCompanyContract = z.object({
   name: z.string().min(1, 'Поле обязательно'),
   active_users_limit: z.union([z.string(), z.number()]).transform(Number),
-  contract_number: z.string().min(1, 'Поле обязательно'),
+  contract_number: z
+    .union([z.string(), z.number()])
+    .transform(value => String(value))
+    .pipe(z.string().min(1, 'Поле обязательно')),
   contract_end_date: z.string().min(1, 'Поле обязательно'),
   can_primary_sales: z.boolean().default(false),
   can_secondary_sales: z.boolean().default(false),
